99bd4b6e21930bf12b3b535e3deb3fba4de061f9
arrayan-project <juanfloresogaz@gmail.com>
2025-01-18 20:42:29 -0300

    Version 2 Zoily

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/zoily_carrero_web",
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
