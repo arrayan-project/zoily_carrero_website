8182b8e3222d42384e4983b607582e7296c8db7d
arrayan-project <juanfloresogaz@gmail.com>
2025-03-21 00:24:24 -0300

    Updates

--- assets/AnimationLayer-BLSrGyyS.js
+++ assets/AnimationLayer-BLSrGyyS.js
@@ -1,4 +1,4 @@
-import{r as Z,j as ft}from"./index-6DRt2FJk.js";/*! @license is-dom-node v1.0.4
+import{r as Z,j as ft}from"./index-CpRc0pX6.js";/*! @license is-dom-node v1.0.4
 
 	Copyright 2018 Fisssion LLC.
 
